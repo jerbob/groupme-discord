6c19c760dcca04bfb3f4fbcca3c8d561432e2b77
Shadowmech88 <shadowmech88@gmail.com>
2019-06-28 01:12:01 -0500

    Adds support for GroupMe attachments.
    Images posted to the GroupMe chat will now be mirrored to Discord, and non-image attachments such as videos will have links to them posted in Discord.
    Also prevents the bot from seeing and posting its own GroupMe posts to Discord, rather than posting and then deleting them.

--- web_server.py
+++ web_server.py
@@ -1,5 +1,6 @@
 """A server-side Flask app to parse POST requests from GroupMe."""
 
+import json
 from json import loads
 from multiprocessing import Process
 
@@ -16,12 +17,35 @@ app = Flask(__name__)
 def index():
     """Method for base route."""
     message_object = loads(request.data)
+    """Prevents the bot from posting its own messages to Discord at all, rather than posting and immediately deleting them."""
+    if message_object['name'] == 'your bots name in groupme':
+        return ''
+    data = {}
+    data['username'] = message_object['name']
+    data['content'] = message_object['text']
+    data['avatar_url'] = message_object['avatar_url']
+    attachment_url = ''
+    attachment_is_image = False
+    attachments = message_object['attachments']
+    if len(attachments) > 0:
+        print(attachments[0].items())
+        if attachments[0]['type'] == 'image':
+            attachment_is_image = True
+           attachment_url = attachments[0]['url']
+        
+    data['embeds'] = []
+    embed = {}
+    if attachment_url:
+        if attached_is_image:
+            embed['image'] = {}
+            embed['image']['url'] = attachment_url
+        else:
+            embed['title'] = message_object['name'],' uploaded a non-image attachment.'
+            embed['url'] = attachment_url
+        data['embeds'].append(embed)
+        
     requests.post(
-        WEBHOOK_URL, data={
-            'username': message_object['name'],
-            'content': message_object['text'],
-            'avatar_url': message_object['avatar_url']
-        }
+        WEBHOOK_URL, data=json.dumps(data), headers={"Content-Type": "application/json"}
     )
     return ''
 
