924fc7f9b711e5a56a5c927e9d7e1fd68d79ccaa
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-07-09 19:53:48 -0400

    redo constants to env var only

--- constants.py
+++ constants.py
@@ -2,26 +2,17 @@
 
 from os import environ
 from typing import Union
-from configparser import ConfigParser
-
-
-config = ConfigParser()
-config.read('config.ini')
 
 
 def get_constant(
     env_name: str, config_header: str, config_key: str
 ) -> Union[str, int]:
     """Get a constant from the environment or config."""
-    constant = environ.get(env_name) or config.get(config_header, config_key)
+    constant = environ.get(env_name) 
     if constant.isnumeric():
         constant = int(constant)
     return constant
 
 
-LOCAL_PORT = get_constant('LOCAL_PORT','flask', 'local_port')
-GROUPME_ID = get_constant('GROUPME_ID', 'groupme', 'bot_id')
 BOT_TOKEN = get_constant('DISCORD_BOT_TOKEN', 'discord', 'bot_token')
-WEBHOOK_URL = get_constant('DISCORD_WEBHOOK', 'discord', 'webhook_url')
 GROUPME_TOKEN = get_constant('GROUPME_TOKEN', 'groupme', 'access_token')
-CHANNEL_NAME = get_constant('DISCORD_CHANNEL_NAME', 'discord', 'channel_name')
--- main.py
+++ main.py
@@ -3,27 +3,31 @@
 import web_server
 import discord_bot
 
-from constants import BOT_TOKEN, GROUPME_TOKEN #set as environment variables
+from constants import BOT_TOKEN, GROUPME_TOKEN  # set as environment variables
+
+#GROUPME_TOKEN=groupme token here
+#DISCORD_BOT_TOKEN=discord bot token
+
 
 settings = [
-     {
-        'bot_id': "60d8f6e70e5fe17ff5e236e072", #groupme
-        'channel_name': "cs_memes", #discord
+    {
+        'bot_id': "60d8f6e70e5fe17ff5e236e072",  # groupme
+        'channel_name': "cs_memes",  # discord
         'webhook_url': "https://discordapp.com/api/webhooks/730924768769081457/wB3rLeUJKnUrfk4clQaaYJ2eVHGm6WhJa2lLICSerHvBl4fYFmYhShaligTWknbK-h88",
-        'local_port': 50070 #server
+        'local_port': 50070  # server
     }
 ]
 for instance in settings:
 
-    GROUPME_ID=instance['bot_id']
-    CHANNEL_NAME=instance['channel_name']
-    WEBHOOK_URL=instance['webhook_url']
-    LOCAL_PORT=instance['local_port']
+    GROUPME_ID = instance['bot_id']
+    CHANNEL_NAME = instance['channel_name']
+    WEBHOOK_URL = instance['webhook_url']
+    LOCAL_PORT = instance['local_port']
 
     flask_options = {}
 
-    if "false" in str(LOCAL_PORT):
-        flask_options = {'host': '0.0.0.0', port: LOCAL_PORT}
+    if "false" not in str(LOCAL_PORT):
+        flask_options = {'host': '0.0.0.0', 'port': LOCAL_PORT}
 
     discord_bot.main(BOT_TOKEN, GROUPME_TOKEN, GROUPME_ID, CHANNEL_NAME)
     web_server.main(WEBHOOK_URL, **flask_options)
