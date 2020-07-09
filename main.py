4f292a42ba47174a9c3048e694fd8fb89d21c415
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-07-09 19:57:37 -0400

    updated credential structrue

--- main.py
+++ main.py
@@ -3,22 +3,10 @@
 import web_server
 import discord_bot
 
-from constants import BOT_TOKEN, GROUPME_TOKEN  # set as environment variables
-
-#GROUPME_TOKEN=groupme token here
-#DISCORD_BOT_TOKEN=discord bot token
+from credentials import *
 
 
-settings = [
-    {
-        'bot_id': "60d8f6e70e5fe17ff5e236e072",  # groupme
-        'channel_name': "cs_memes",  # discord
-        'webhook_url': "https://discordapp.com/api/webhooks/730924768769081457/wB3rLeUJKnUrfk4clQaaYJ2eVHGm6WhJa2lLICSerHvBl4fYFmYhShaligTWknbK-h88",
-        'local_port': 50070  # server
-    }
-]
 for instance in settings:
-
     GROUPME_ID = instance['bot_id']
     CHANNEL_NAME = instance['channel_name']
     WEBHOOK_URL = instance['webhook_url']
@@ -27,7 +15,7 @@ for instance in settings:
     flask_options = {}
 
     if "false" not in str(LOCAL_PORT):
-        flask_options = {'host': '0.0.0.0', 'port': LOCAL_PORT}
+        flask_options = {'host': '127.0.0.1', 'port': LOCAL_PORT}
 
     discord_bot.main(BOT_TOKEN, GROUPME_TOKEN, GROUPME_ID, CHANNEL_NAME)
     web_server.main(WEBHOOK_URL, **flask_options)
